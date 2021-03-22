ff92419202445a6cbd792a644b08079f65d78fe0
Edgar Ortiz <ed.ortizm@gmail.com>
2021-03-22 18:31:43 -0300

    Building hiden layer before sampling layer using a for loop in class DenseEncoder. Pending to implement the sampling layer to finish the class

--- lib_VAE_outlier.py
+++ lib_VAE_outlier.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import chisquare
 from sklearn.decomposition import PCA
+################################################################################
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import backend as K
@@ -15,7 +16,69 @@ from tensorflow.keras.utils import plot_model
 
 from constants_AEs import sdss_data_proc
 
+################################################################################
 class VAE:
+    """ VAE for outlier detection using tf.keras
+
+    References:
+    Portillo et al. 2020
+    """
+
+    def __init__(self, encoder:'keras.model', decoder:'keras.model'):
+        pass
+################################################################################
+class DenseEncoder:
+    def __init__(self, n_input_dimensions:'int', n_hiden_layers:'list',
+        n_latent_dimensions:'int')->'keras.model':
+
+        self.n_input_dimensions = n_input_dimensions
+        self.n_hiden_layers = n_hiden_layers
+        self.n_latent_dimensions = n_latent_dimensions
+
+    def stocastic_layer(self):
+
+        # w_init = keras.initializers.RandomNormal(mean=0.,
+        # stddev=np.sqrt(2./self.hid_dim[1]))
+        # latent_mu = Dense(self.hid_dim[2], name='latent_mu',
+        # kernel_initializer=w_init)(hidden_1)
+        # latent_ln_sigma = Dense(self.hid_dim[2], name='latent_ln_sigma',
+        # kernel_initializer=w_init)(hidden_1)
+        #
+        # latent = Lambda(self._sample_latent_features,
+        # output_shape=(self.hid_dim[2],),
+        # name='latent')([latent_mu, latent_ln_sigma])
+        #
+        #
+        # self.encoder = Model(inputs, latent, name='encoder')
+        # self.encoder.summary()
+        # # plot_model(self.encoder, to_file='encoder.png', show_shapes='True')
+        pass
+        
+    def build_encoder(self):
+
+        inputs = Input(shape=(self.n_input_dimensions,), name='encoder_input')
+        std_dev = np.sqrt(2./self.n_input_dimensions)
+
+        X = inputs
+        for idx, n_units in enumerate(self.n_hiden_layers):
+
+            w_init = keras.initializers.RandomNormal(mean=0., stddev=std_dev)
+            std_dev = np.sqrt(2./n_units)
+
+            layer = Dense(n_units, name=f'layer_{idx:02},
+                activation='relu', kernel_initializer=w_init)(X)
+
+            X = layer
+# Now comes the stocastic layer
+        pass
+
+################################################################################
+class DenseDecoder:
+    def __init__(self):
+        pass
+
+################################################################################
+class TestVAE:
     """ VAE for outlier detection using tf.keras
 
     References:
@@ -39,14 +102,6 @@ class VAE:
         self.encoder = None
         self.decoder = None
         self.VAE = None
-        self._create_VAE()
-
-        #
-        # def _init_VAE(self):
-        #     # blabla
-        #
-        #     # Create AE
-        #     self._create_VAE()
 
     def _create_VAE(self):
 
@@ -148,7 +203,313 @@ class VAE:
 
         # return K.mean(vae_loss)
         return vae_loss
+###############################################################################
+class Outlier:
+    """
+    Class for dealing with the outliers based on a generative model trained with
+    tensorflow.keras
+    """
 
+    def __init__(self, model_path, o_scores_path='.', metric='mse', p='p',
+        custom=False, custom_metric=None):
+        """
+        Init fucntion
+
+        Args:
+            model_path: path where the trained generative model is located
+
+            o_scores_path: (str) path where the numpy arrays with the outlier scores
+                is located. Its functionality is for the cases where the class
+                will be implemented several times, offering therefore the
+                possibility to load the scores instead of computing them over
+                and over.
+
+            metric: (str) the name of the metric used to compute the outlier score
+                using the observed spectrum and its reconstruction. Possible
+
+            p: (float > 0) in case the metric is the lp metric, p needs to be a non null
+                possitive float [Aggarwal 2001]
+        """
+
+        self.model_path = model_path
+        self.o_scores_path = o_scores_path
+        self.metric = metric
+        self.p = p
+        self.custom = custom
+        if self.custom:
+            self.custom_metric = custom_metric
+
+    def _get_OR(self, O, model):
+
+        if len(O.shape) == 1:
+            O = O.reshape(1,-1)
+
+        R = model.predict(O)
+
+        return O, R
+
+    def score(self, O):
+        """
+        Computes the outlier score according to the metric used to instantiate
+        the class.
+
+        Args:
+            O: (2D np.array) with the original objects where index 0 indicates
+            the object and index 1 the features of the object.
+
+        Returns:
+            A one dimensional numpy array with the outlier scores for objects
+            present in O
+        """
+
+        model_name = self.model_path.split('/')[-1]
+        print(f'Loading model: {model_name}')
+        model = load_model(f'{self.model_path}')
+
+        O, R = self._get_OR(O, model)
+
+        if self.custom:
+            print(f'Computing the predictions of {model_name}')
+            return self.user_metric(O=O, R=R)
+
+        elif self.metric == 'mse':
+            print(f'Computing the predictions of {model_name}')
+            return self._mse(O=O, R=R)
+
+        elif self.metric == 'chi2':
+            print(f'Computing the predictions of {model_name}')
+            return self._chi2(O=O, R=R)
+
+        elif self.metric == 'mad':
+            print(f'Computing the predictions of {model_name}')
+            return self._mad(O=O, R=R)
+
+        elif self.metric == 'lp':
+
+            if self.p == 'p' or self.p <= 0:
+                print(f'For the {self.metric} metric you need p')
+                return None
+
+            print(f'Computing the predictions of {model_name}')
+            return self._lp(O=O, R=R)
+
+        else:
+            print(f'The provided metric: {self.metric} is not implemented yet')
+            return None
+
+    def _coscine_similarity(self, O, R):
+        """
+        Computes the coscine similarity between the reconstruction of the input
+        objects
+
+        Args:
+            O: (2D np.array) with the original objects where index 0 denotes
+                indicates the objec and index 1 the features of the object.
+
+            R: Reconstruction of O by (tensorflow.keras model) the generative
+            model
+
+        Returns:
+            A one dimensional numpy array with the cosine similarity between
+            objects O and their reconstructiob
+        """
+
+        pass
+
+    def _jaccard_index(self, O, R):
+        """
+        Computes the mean square error for the reconstruction of the input
+        objects
+
+        Args:
+            O: (2D np.array) with the original objects where index 0 denotes
+                indicates the objec and index 1 the features of the object.
+
+            R: Reconstruction of O by (tensorflow.keras model) the generative
+            model
+
+        Returns:
+            A one dimensional numpy array with the mean square error for objects
+            present in O
+        """
+
+        pass
+
+    def _sorensen_dice_index(self, O, R):
+        """
+        Computes the mean square error for the reconstruction of the input
+        objects
+
+        Args:
+            O: (2D np.array) with the original objects where index 0 denotes
+                indicates the objec and index 1 the features of the object.
+
+            R: Reconstruction of O by (tensorflow.keras model) the generative
+            model
+
+        Returns:
+            A one dimensional numpy array with the mean square error for objects
+            present in O
+        """
+        pass
+# Mahalanobis, Canberra, Braycurtis, and KL-divergence
+    def _mse(self, O, R):
+        """
+        Computes the mean square error for the reconstruction of the input
+        objects
+
+        Args:
+            O: (2D np.array) with the original objects where index 0 denotes
+                indicates the objec and index 1 the features of the object.
+
+            R: Reconstruction of O by (tensorflow.keras model) the generative
+            model
+
+        Returns:
+            A one dimensional numpy array with the mean square error for objects
+            present in O
+        """
+
+        return np.square(R-O).mean(axis=1)
+
+    def _chi2(self, O, R):
+        """
+        Computes the chi square error for the reconstruction of the input
+        objects
+
+        Args:
+            O: (2D np.array) with the original objects where index 0 denotes
+                indicates the objec and index 1 the features of the object.
+
+            R: Reconstruction of O by (tensorflow.keras model) the generative
+            model
+
+        Returns:
+            A one dimensional numpy array with the chi square error for objects
+            present in O
+        """
+
+        return (np.square(R-O)*(1/np.abs(R))).mean(axis=1)
+
+    def _mad(self, O, R):
+        """
+        Computes the maximum absolute deviation from the reconstruction of the
+        input objects
+
+        Args:
+            O: (2D np.array) with the original objects where index 0 denotes
+                indicates the objec and index 1 the features of the object.
+
+            R: Reconstruction of O by (tensorflow.keras model) the generative
+            model
+
+        Returns:
+            A one dimensional numpy array with the maximum absolute deviation
+            from the objects present in O
+        """
+
+        return np.abs(R-O).mean(axis=1)
+
+    def _lp(self, O, R):
+        """
+        Computes the lp distance from the reconstruction of the input objects
+
+        Args:
+            O: (2D np.array) with the original objects where index 0 denotes
+                indicates the objec and index 1 the features of the object.
+
+            R: Reconstruction of O by (tensorflow.keras model) the generative
+            model
+
+        Returns:
+            A one dimensional numpy array with the lp distance from the objects
+            present in O
+        """
+
+        return (np.sum((np.abs(R-O))**self.p, axis=1))**(1/self.p)
+# gotta code conditionals to make sure that the user inputs a "good one"
+    def user_metric(self, custom_metric, O, R):
+        """
+        Computes the custom metric for the reconstruction of the input objects
+        as defined by the user
+
+        Args:
+            O: (2D np.array) with the original objects where index 0 denotes
+                indicates the objec and index 1 the features of the object.
+
+            R: Reconstruction of O by (tensorflow.keras model) the generative
+            model
+
+        Returns:
+            A one dimensional numpy array with the score produced by the user
+            defiend metric of objects present in O
+        """
+
+        return self.custom_metric(O, R)
+
+    def metadata(self, spec_idx, training_data_files):
+        """
+        Generates the names and paths of the individual objects used to create
+        the training data set.
+        Note: this work according to the way the training data set was created
+
+        Args:
+            spec_idx: (int > 0) the location index of the spectrum in the
+                training data set.
+
+            training_data_files: (list of strs) a list with the paths of the
+                individual objects used to create the training data set.
+
+        Returns:
+            sdss_name, sdss_name_path: (str, str) the sdss name of the objec,
+                the path of the object in the files system
+        """
+
+
+        # print('Gathering name of data points used for training')
+
+        sdss_names = [name.split('/')[-1].split('.')[0] for name in
+            training_data_files]
+
+        # print('Retrieving the sdss name of the desired spectrum')
+
+        sdss_name = sdss_names[spec_idx]
+        sdss_name_path = training_data_files[spec_idx]
+
+        return sdss_name, sdss_name_path
+
+    def top_reconstructions(self, O, n_top_spectra):
+        """
+        Selects the most normal and outlying objecs
+
+        Args:
+            O: (2D np.array) with the original objects where index 0 denotes
+                indicates the objec and index 1 the features of the object.
+
+            n_top_spectra: (int > 0) this parameter controls the number of
+                objects identifiers to return for the top reconstruction,
+                that is, the idices for the most oulying and the most normal
+                objects.
+
+        Returns:
+            most_normal, most_oulying: (1D np.array, 1D np.array) numpy arrays
+                with the location indexes of the most normal and outlying
+                object in the training (and pred) set.
+        """
+
+        if os.path.exists(f"{self.o_scores_path}/{self.metric}_o_score.npy"):
+            scores= np.load(f"{self.o_scores_path}/{self.metric}_o_score.npy")
+        else:
+            scores = self.score(O)
+
+        spec_idxs = np.argpartition(scores,
+        [n_top_spectra, -1*n_top_spectra])
+
+        most_normal_ids = spec_idxs[: n_top_spectra]
+        most_oulying_ids = spec_idxs[-1*n_top_spectra:]
+
+        return most_normal_ids, most_oulying_ids
+###############################################################################
 class AEpca:
 
     def __init__(self, in_dim, lat_dim=2, batch_size=32, epochs=10, lr= 1e-4):
@@ -210,60 +571,7 @@ class AEpca:
         self.encoder.save('encoder')
         self.decoder.save('decoder')
         self.AE.save('AutoEncoder')
-
-class Outlier:
-
-    def __init__(self, dbPath=sdss_data_proc, N=20):
-        self.scores = None
-        self.N = N
-        self.fnames = glob(f'{dbPath}/*')
-
-    def chi2(self, O, P):
-        self.scores = (np.square(P-O)*(1/np.abs(P))).mean(axis=1)
-        np.save('chi2_outlier_scores.npy', self.scores)
-        self.retrieve(metric='chi2')
-
-    def mse(self, O, P):
-        self.scores = np.square(P-O).mean(axis=1)
-        np.save('mse_outlier_scores.npy', self.scores)
-        self.retrieve(metric='mse')
-
-
-    def mad(self, O, P):
-        self.scores = np.abs(P-O).mean(axis=1)
-        np.save('mad_outlier_scores.npy', self.scores)
-        self.retrieve(metric='mad')
-
-    def lp(self, O, P, p=1):
-        self.scores = (np.sum((np.abs(P-O))**p, axis=1))**(1/p)
-        np.save(f'lp_{p}_outlier_scores.npy', self.scores)
-        self.retrieve(metric=f'lp_{p}')
-
-    def area(self, O, P):
-        self.scores = np.trapz(np.square(P-O), axis=1)
-        np.save('area_outlier_scores.npy', self.scores)
-        self.retrieve(metric=f'area')
-
-    def lpf(self, O, P, p=1):
-        self.scores = (np.trapz((np.abs(P-O))**p, axis=1))**(1/p)
-        np.save(f'lpf_{p}_outlier_scores.npy', self.scores)
-        self.retrieve(metric=f'lpf_{p}')
-
-
-    def retrieve(self, metric=None, fraction=1):
-        print('Loading outlier scores!')
-
-        ids_proc_specs = np.argpartition(self.scores, -1*int(self.N*fraction))[-1*int(self.N*fraction):]
-        np.save(f'outlier_ids_{metric}', ids_proc_specs)
-        print('Outlier scores for the weirdest spectra')
-
-        names = open(f'{metric}_fnames.txt', 'w+')
-
-        for n, idx in enumerate(ids_proc_specs):
-            print(f'ID:{idx} --> {self.scores[idx]}. File name: {self.fnames[idx].split("/")[-1]}', end='\r')
-            names.write(f'{self.fnames[idx].split("/")[-1][:-4]}\n')
-        names.close()
-
+###############################################################################
 class PcA:
 
     def __init__(self, n_comps = False):
@@ -293,7 +601,7 @@ class PcA:
             test_spec = test_spec.reshape(1, -1)
 
         return self.PCA.transform(test_spec)
-
+###############################################################################
 def plt_spec_pca(flx,pca_flx,componets):
     '''Comparative plot to see how efficient is the PCA compression'''
     plt.figure(figsize=(8,4));
@@ -311,7 +619,7 @@ def plt_spec_pca(flx,pca_flx,componets):
     plt.title('Reconstructed spectra', fontsize = 20)
     plt.show()
     plt.close()
-
+###############################################################################
 def plot_2D(data, title):
     fig = plt.figure()
     plt.title(title)
@@ -321,3 +629,4 @@ def plot_2D(data, title):
     plt.savefig(f'{title}.png')
     plt.show()
     plt.close()
+###############################################################################
