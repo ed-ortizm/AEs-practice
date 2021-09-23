6759dca69f31baef2b3c5415ec17875dea8b1e26
Edgar Ortiz <ed.ortizm@gmail.com>
2021-09-23 11:20:49 -0300

    restructuring directory tree

--- variational/cvae/train.py
+++ variational/cvae/train.py
@@ -6,7 +6,7 @@ import time
 
 ################################################################################
 import numpy as np
-
+import json
 ################################################################################
 parser = ConfigParser(interpolation=ExtendedInterpolation())
 parser.read("convolutional.ini")
@@ -15,25 +15,39 @@ parser.read("convolutional.ini")
 work_directory = parser.get("directories", "work")
 sys.path.insert(0, f"{work_directory}")
 ################################################################################
-from src.vae import VAE
+from src import variational
 
 ################################################################################
 ti = time.time()
 ################################################################################
 # load data
-data_directory = parser.get("directories", "train")
-data_name = parser.get("files", "train")
-data = np.load(f"{data_directory}/{data_name}")
-input_dimensions = data.shape[1]
+# data_directory = parser.get("directories", "train")
+# data_name = parser.get("files", "train")
+# data = np.load(f"{data_directory}/{data_name}")
+# input_dimensions = data.shape[1]
 ############################################################################
 # network architecture
-encoder_units = parser.get("architecture", "encoder")
-encoder_units = [int(units) for units in encoder_units.split("_")]
-
-latent_dimensions = parser.getint("architecture", "latent_dimensions")
-
-decoder_units = parser.get("architecture", "decoder")
-decoder_units = [int(units) for units in decoder_units.split("_")]
+input_shape = parser.get("architecture", "input")
+input_shape = json.loads(input_shape)
+print(input_shape)
+# encoder_filters =
+# encoder_kernels =
+# encoder_strides =
+# latent_dimensions =
+# decoder_filters =
+# decoder_kernels =
+# decoder_strides =
+# batch_size =
+# epochs =
+# learning_rate =
+# reconstruction_loss_weight =
+# encoder_units = parser.get("architecture", "encoder")
+# encoder_units = [int(units) for units in encoder_units.split("_")]
+#
+# latent_dimensions = parser.getint("architecture", "latent_dimensions")
+#
+# decoder_units = parser.get("architecture", "decoder")
+# decoder_units = [int(units) for units in decoder_units.split("_")]
 ############################################################################
 # network hyperparameters
 learning_rate = parser.getfloat("hyper-parameters", "learning_rate")
@@ -44,26 +58,26 @@ reconstruction_weight = parser.getint(
     "hyper-parameters", "reconstruction_weight"
 )
 ################################################################################
-vae = VAE(
-    input_dimensions=input_dimensions,
-    encoder_units=encoder_units,
-    latent_dimensions=latent_dimensions,
-    decoder_units=decoder_units,
-    batch_size=batch_size,
-    epochs=epochs,
-    learning_rate=learning_rate,
-    reconstruction_loss_weight=reconstruction_weight,
-)
-
-vae.summary()
+# vae = VAE(
+#     input_dimensions=input_dimensions,
+#     encoder_units=encoder_units,
+#     latent_dimensions=latent_dimensions,
+#     decoder_units=decoder_units,
+#     batch_size=batch_size,
+#     epochs=epochs,
+#     learning_rate=learning_rate,
+#     reconstruction_loss_weight=reconstruction_weight,
+# )
+#
+# vae.summary()
 ################################################################################
-# Training the model
-history = vae.train(data)
-# save model
-models_directory = parser.get("directories", "models")
-
-if not os.path.exists(models_directory):
-    os.makedirs(models_directory)
+# # Training the model
+# history = vae.train(data)
+# # save model
+# models_directory = parser.get("directories", "models")
+#
+# if not os.path.exists(models_directory):
+#     os.makedirs(models_directory)
 ###############################################################################
 # save the model once it is trained
 
